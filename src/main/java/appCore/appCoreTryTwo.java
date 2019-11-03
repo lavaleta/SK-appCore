47a62d61dbb230e49c4c22b0f425ecaf91a87206
Lavaleta <lavaletars@gmail.com>
2019-11-03 20:31:20 +0100

    Prvi komit ikadaaaa

--- src/main/java/appCore/appCoreTryTwo.java
+++ src/main/java/appCore/appCoreTryTwo.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class appCoreTryTwo {
 
     private SpecInterface dropBoxObj;
-
+    
     public static void main(String[] args) {
         new appCoreTryTwo().runApp();
     }
